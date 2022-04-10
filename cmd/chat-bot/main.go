458ce34c8d8e8136a136f214897cc03a39fc4022
Kirill <kirill1997_markin@mail.ru>
2022-04-10 14:18:34 +0300

    Fix conf key

--- cmd/chat-bot/main.go
+++ cmd/chat-bot/main.go
@@ -37,6 +37,7 @@ func main() {
 	// Подключаемся к API VK
 	vk := api.NewVK(cfg.ApiVK.ApiKey)
 	vkCallback := callback.NewCallback()
+	vkCallback.ConfirmationKey = "bc90fc38"
 	vkCallback.Title = "Bot"
 
 	conn := vkConn.NewConnection(vk)
